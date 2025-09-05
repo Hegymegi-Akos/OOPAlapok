bfebf8a16030447508eb191b25ce4409a95e6834
HKÁkos <hegymegi-kissa@kkszki.hu>
2025-09-05 11:26:22 +0200

    feladat is ready

--- oooalapok/Program.cs
+++ oooalapok/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace oooalapok
 {
+    public class Szemely
+    { 
+        public string nev = "Kiss Péter ";
+        public int kor = 35;
+
+
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+
+            Szemely tanulo = new Szemely();
+            Console.WriteLine(tanulo.nev);
+            Console.WriteLine(tanulo.kor);
+
         }
     }
 }
